dab45784f8bce9eacce0f06676c8d3bbf31fae00
Ana Juárez <lisa13suarez@gmail.com>
2023-06-10 23:16:29 -0600

    💻 Show data of cards

---
+++ src/app/pages/list/list.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+
+import { Card } from 'src/app/interfaces/card.interface';
+import { CardService } from 'src/app/services/card.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+
+export class ListComponent {
+
+  cards: Card[] = []
+  offset = 0
+
+  cardTextFC = new FormControl('Frog')
+
+  constructor(private cardService: CardService) {}
+
+  ngOnInit(): void {
+    this.cardTextFC.valueChanges.pipe(debounceTime(1000))
+    .subscribe(res => {
+      console.log(res)
+      this.cards = []
+      this.searchCards(res)
+    })
+    this.searchCards()
+  }
+
+  onScroll() {
+    console.log("scrolled!!");
+    this.offset += 100
+    this.searchCards()
+  }
+
+  searchCards(cardName: string | null = null) {
+    this.cardService.getCards(cardName, this.offset).subscribe(res => {
+      console.log(res)
+      this.cards = [...this.cards, ...res]
+    })
+  }
+}
